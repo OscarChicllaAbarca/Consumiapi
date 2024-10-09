fdb87953c1a31c2bafcb71a4651421e71ceca249
OscarChicllaAbarca <oscarchicabar@gmail.com>
2024-10-09 09:52:02 -0500

    exito en la toma de produc

--- src/views/TomaView.vue
+++ src/views/TomaView.vue
@@ -7,10 +7,10 @@
         <input type="date" v-model="productData.fechaToma" required disabled>
 
         <label for="id_producto">Ubicacion:</label>
-        <input type="text" v-model="productData.ubicacion" @input="fetchHannaData" required placeholder="Ingrese el ID del producto">
+        <input type="text" v-model="productData.ubicacion" @input="fetchHannaData" required placeholder="00-00-000-000">
 
         <label for="producto">Producto:</label>
-        <input type="text" v-model="productData.producto" required readonly>
+        <input type="text" v-model="productData.producto" required>
 
         <label for="descripcion">Descripción de Producto:</label>
         <input type="text" v-model="productData.descripcionProducto" required readonly>
